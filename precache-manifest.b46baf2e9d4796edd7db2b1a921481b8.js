33e481f93e6b2eba611d5015ff04d9acdc8a6d04
marinamejj123 <marinamejj123@gmail.com>
2022-12-08 10:51:54 -0400

    Updates

---
+++ precache-manifest.b46baf2e9d4796edd7db2b1a921481b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04755cfc004940c44f44dd97c4196f56",
+    "url": "/chat01/index.html"
+  },
+  {
+    "revision": "1319bedd7c4d32f5e1d4",
+    "url": "/chat01/static/css/2.8e2610c1.chunk.css"
+  },
+  {
+    "revision": "f355dd7a5f283f1e5740",
+    "url": "/chat01/static/css/main.b3efca0b.chunk.css"
+  },
+  {
+    "revision": "1319bedd7c4d32f5e1d4",
+    "url": "/chat01/static/js/2.dfce0dca.chunk.js"
+  },
+  {
+    "revision": "04ccf57cace4e7ac842705b412ebf9df",
+    "url": "/chat01/static/js/2.dfce0dca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f355dd7a5f283f1e5740",
+    "url": "/chat01/static/js/main.72dcc180.chunk.js"
+  },
+  {
+    "revision": "6ff514221a77f582eb7b",
+    "url": "/chat01/static/js/runtime-main.3a395c7f.js"
+  }
+]);
